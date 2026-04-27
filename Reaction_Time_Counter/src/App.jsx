b8f8b1b5e88800c1215e6ba7259e38d5b2da1a37
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-04-27 23:54:54 +0530

    feat: implement initial reaction time tracking functionality in App component

--- Reaction_Time_Counter/src/App.jsx
+++ Reaction_Time_Counter/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+const [status, setStatus] = useState('idle')
+const [startTime, setStartTime] = useState(null)
+const [reactionTime, setReactionTime] = useState(null)
+
   return (
-    <div>App</div>
+    <div>
+      { status === 'idle' && (
+        <div>
+          <h1>Reaction Speed Time</h1>
+          <button onClick={()=> setStatus(countdown)}>Start Code</button>
+        </div>
+      )}
+    </div>
   )
 }
 
